dd89444c74a579808a0620adfc816ce7e7af0265
VaneRojo <vanerojo1993@gmail.com>
2022-08-21 18:12:24 -0300

    Java
    Se comienza con el backend

---
+++ portfolioWeb/BackEnd/src/test/java/com/portfolioWeb/rvs/RvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioWeb.rvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
