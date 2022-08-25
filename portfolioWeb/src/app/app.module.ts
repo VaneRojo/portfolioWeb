845814228286a5ce847ea7ce79aaf3ab654a0321
VaneRojo <vanerojo1993@gmail.com>
2022-08-25 11:06:30 -0300

    PersonaController

--- portfolioWeb/src/app/app.module.ts
+++ portfolioWeb/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
