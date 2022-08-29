884c71a9105c87f8761bcb9d7d677226b942ce90
VaneRojo <vanerojo1993@gmail.com>
2022-08-29 09:26:53 -0300

    Update edit-acerca-de.component.ts

--- portfolioWeb/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ portfolioWeb/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -10,7 +10,7 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 export class EditAcercaDeComponent implements OnInit {
 
-  persona: persona = null!;
+  persona: persona = [];
 
   constructor(private sPersona: PersonaService, private activatedRouter: ActivatedRoute, private router: Router) { }
 
