d94aa6d4d95a9d9f59f0eb83a4025e4c01dd63b6
VaneRojo <vanerojo1993@gmail.com>
2022-10-31 12:19:24 -0300

    Update

--- portfolioWeb/BackEnd/BackEnd/src/main/java/com/portfolioWeb/rvs/Controller/CExperiencia.java
+++ portfolioWeb/BackEnd/BackEnd/src/main/java/com/portfolioWeb/rvs/Controller/CExperiencia.java
@@ -4,6 +4,16 @@
  */
 package com.portfolioWeb.rvs.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.portfolioWeb.rvs.Dto.dtoExperiencia;
 import com.portfolioWeb.rvs.Entity.Experiencia;
 import com.portfolioWeb.rvs.Security.Controller.Mensaje;
@@ -14,20 +24,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  *
  * @author vane_
  */
-@Controller
+@RestController
 @RequestMapping("explab")
 @CrossOrigin(origins = "https://front-end-pw.web.app")
 public class CExperiencia {
